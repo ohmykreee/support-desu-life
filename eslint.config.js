edf805b23e56b445b58d4265366c13aaba87b6f6
Kyree Jonas <contact@kre3.net>
2026-04-09 21:52:40 +0800

    Feat: add ts/svelte lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,42 @@
 import eslintPluginAstro from "eslint-plugin-astro";
+import { includeIgnoreFile } from "@eslint/compat";
+import path from "node:path";
+import js from "@eslint/js";
+import svelte from "eslint-plugin-svelte";
+import ts from "typescript-eslint";
+import prettier from "eslint-config-prettier";
+import svelteConfig from "./svelte.config.js";
+
+const gitignorePath = path.resolve(import.meta.dirname, ".gitignore");
+
 export default [
   // add more generic rule sets here, such as:
   // js.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
+  includeIgnoreFile(gitignorePath),
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  ...svelte.configs.recommended,
+  prettier,
+  ...svelte.configs.prettier,
   {
     rules: {
+      // typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
+      // see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+      "no-undef": "off"
       // override/add rules settings here, such as:
       // "astro/no-set-html-directive": "error"
+    }
+  },
+  {
+    files: ["**/*.svelte", "**/*.svelte.ts", "**/*.svelte.js"],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        extraFileExtensions: [".svelte"],
+        parser: ts.parser,
+        svelteConfig
+      }
     }
   }
 ];
