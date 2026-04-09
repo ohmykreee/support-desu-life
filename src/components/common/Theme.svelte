d1e82fe44ac9e9c2be760b52ec09ec0541a54d83
Kyree Jonas <contact@kre3.net>
2026-04-09 13:58:23 +0800

    Feat: add Theme button

---
+++ src/components/common/Theme.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+  import { untrack } from "svelte";
+  import Icon from "@iconify/svelte";
+
+  type Theme = 'light' | 'dark';
+
+  let theme = $state();
+
+  $effect(() => {
+    const savedTheme = localStorage.getItem('theme');
+    let currTheme: Theme;
+
+    if (theme === 'light' || theme === 'dark') {
+      currTheme = $state.snapshot(theme);
+    } else if (savedTheme === 'light' || savedTheme === 'dark') {
+      currTheme = savedTheme;
+    } else if (window.matchMedia('(prefers-color-scheme: light)').matches) {
+      currTheme = 'light';
+    } else {
+      currTheme = 'dark';
+    };
+
+    if (currTheme === 'light') {
+      document.body.classList.add('light');
+      localStorage.setItem('theme', $state.snapshot(theme));
+    } else {
+      document.body.classList.remove('light');
+      localStorage.setItem('theme', $state.snapshot(theme));
+    }
+    
+    untrack(() => {
+      theme = currTheme;
+    });
+    console.log('triggered: ' + $state.snapshot(theme))
+  })
+</script>
+
+<button 
+  class="theme-btn" 
+  aria-label="切换主题"
+  onclick={() => {theme = theme === 'dark'? 'light':'dark'}}>
+  {#if theme === 'dark'}
+    <Icon icon="tabler:moon" />
+  {:else}
+    <Icon icon="tabler:sun" />
+  {/if}
+</button>
+
+<style>
+  .theme-btn {
+    cursor: pointer;
+    font-size: 1.25rem;
+  }
+
+  .theme-btn:hover {
+    color: var(--color-primary);
+  }
+</style>
