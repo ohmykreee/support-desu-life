5feb02a41c41557f9159c3d0d9c485f68f770cd7
Kyree Jonas <contact@kre3.net>
2026-04-11 20:58:54 +0800

    Fix: use image.src as key in Swiper.svelte

--- src/components/common/Swiper.svelte
+++ src/components/common/Swiper.svelte
@@ -14,8 +14,8 @@
 </script>
 
 <swiper-container navigation="true" scrollbar="true" autoplay="true" loop="true">
-  <!-- TODO: Ugly fix: 尽量不要用自增 i 作为 each 的 key -->
-  {#each images as image, i (i)}
+  <!-- 因为这里使用 image.src 作为唯一 key，故产品图片名称不要使用同样的名字，不然会渲染错误 -->
+  {#each images as image (image.src)}
     <swiper-slide class="swiper-slide">
       <img src={image.src} alt={image.alt} class="swiper-slide-img" />
     </swiper-slide>
